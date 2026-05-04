db715f72b01a538ad92238c04e6374ab4866c1e0
Alice Index <hh48691537@gmail.com>
2026-05-04 15:05:32 +0900

    edit files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   // あなたのGitHubのURL
   site: 'https://AliceIndex.github.io',
+
   // リポジトリ名が alice-portfolio の場合、末尾のスラッシュを忘れずに
   base: '/alice-portfolio',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
